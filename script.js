ee3726a45db260d8345c746fa60f5cfe779bac87
Pavel <sosnovskiy.pavel@inbox.ru>
2022-09-11 19:22:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+    //Пример 1
     function externalFunc() {
         const variableExt = 'Внешняя функция';
 
@@ -12,4 +13,29 @@
     const internal = externalFunc();
     internal();
 
-    
+    //Пример 2
+    function createAddress (type) {
+        const addressUpCase = type.toUpperCase(); 
+        return function(name){
+            return `${addressUpCase} ${name}`
+        }
+    }
+
+    const addressMan = createAddress('гражданин')
+    const addressWoman = createAddress('гражданка')
+    console.log(addressMan('Сергей Петрович'))
+    console.log(addressWoman('Алла Сергеевна'))
+
+    //Пример 3
+    const counterPoint = (name) => {
+        let point = 0;
+        return function(){
+            point++;
+            return `${name} - ${point}`
+        } 
+    }
+
+    const firstPlayer = counterPoint('Сергей');
+    firstPlayer()
+    const secondPlayer = counterPoint('Сергей');
+    secondPlayer()
