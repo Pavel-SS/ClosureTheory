e6b87b9b00204bd82664503a18ab81969d617682
Pavel <sosnovskiy.pavel@inbox.ru>
2022-09-13 00:17:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,11 @@
 
    bind(personFirst, personFn)()
    bind(personSecond, personFn)()
+
+
+   var initialValue = 0;
+   var sum = [{x:1},{x:2},{x:3}].reduce(
+    (accumulator, currentValue) => accumulator + currentValue.x, initialValue
+   )
+
+   console.log(sum)
